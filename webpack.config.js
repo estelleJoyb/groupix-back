2d1656973e5abb442dfd74f9e4f2d4fb8fcc4528
JeejaFR <jeremy.defuentes@gmail.com>
2024-10-09 15:18:34 +0200

    fix path entry webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './backend/server.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
